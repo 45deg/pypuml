63dfd1a6be3c1ef34f634efdcebd4c6c72700ee9
45deg <45deg@users.noreply.github.com>
2020-03-16 10:09:43 +0900

    Refactored

--- pypuml/__init__.py
+++ pypuml/__init__.py
@@ -1,9 +1,1 @@
 __version__ = "0.1.0"
-
-import sys
-from pypuml.inspector import Project
-
-if __name__ == "__main__":
-    project = Project()
-    project.load_from_file(sys.argv[1])
-    project.generate()
---
+++ pypuml/app.py
@@ -0,0 +1,106 @@
+from astroid import modutils, node_classes
+from astroid.manager import AstroidManager
+import astroid.nodes as ast_node
+import os
+import sys
+
+def is_target_module(mod_name: str):
+    return mod_name != "builtins"
+
+
+class ModuleVisitor(object):
+    def __init__(self):
+        self._manager = AstroidManager()
+        self._manager._transform.transforms = []
+        self.output = []
+        self.child_modules = []
+        self.indent = 0
+        self.file = None
+        self.blacklist = []
+
+    def exec(self, file):
+        self.file = file
+        print("@startuml")
+        self.load_module().accept(self)
+        print("@enduml")
+
+    def walk_child_modules(self):
+        for child_path in modutils.get_module_files(self.file, self.blacklist):
+            orig_file = self.file
+            self.file = child_path
+            yield self.load_module()
+            self.file = orig_file
+
+    def load_module(self):
+        if os.path.isdir(self.file):
+            fpath = os.path.join(self.file, "__init__.py")
+        elif os.path.exists(self.file):
+            fpath = self.file
+        else:
+            fpath = modutils.file_from_modpath(self.file.split("."))
+
+        return self._manager.ast_from_file(fpath)
+
+    def write(self, line: str):
+        print("    " * self.indent + line)
+
+    def visit_module(self, ast: ast_node.Module):
+        self.write("namespace %s {" % ast.name)
+        self.indent += 1
+
+        for child in ast.body:
+            child.accept(self)
+
+        if ast.package:
+            for child_mod in self.walk_child_modules():
+                child_mod.accept(self)
+
+        self.indent -= 1
+        self.write("}")
+
+    def visit_classdef(self, ast: ast_node.ClassDef):
+
+        self.write("class %s {" % ast.name)
+        self.indent += 1
+
+        for instance_attrs in ast.instance_attrs.values():
+            self.write(self._handle_instance_attr(instance_attrs[0]))
+
+        for local in ast.values():
+            local.accept(self)
+
+        self.indent -= 1
+        self.write("}")
+
+        for anc in ast.ancestors(recurs=False):
+            cls = next(anc.infer())
+            if isinstance(cls, ast_node.ClassDef):
+                module = cls.root().name
+                if is_target_module(module):
+                    self.write("%s <|- %s.%s" % (ast.name, cls.root().name, cls.name))
+
+    def visit_functiondef(self, ast: ast_node.FunctionDef):
+        self.write("%s()" % ast.name)
+
+    def visit_assignname(self, ast: ast_node.AssignName):
+        self.write("{static} %s" % ast.name)
+
+    def visit_annassign(self, ast: ast_node.AnnAssign):
+        self.write("{static} %s : %s" % (ast.name, ast.annotation.as_string()))
+
+    def _handle_instance_attr(self, instance_attr: node_classes.NodeNG) -> str:
+        if isinstance(instance_attr, ast_node.AssignAttr):
+            return instance_attr.attrname
+        elif isinstance(instance_attr.parent, ast_node.AnnAssign):
+            # cf. astroid/brain/brain_dataclasses.py
+            return instance_attr.parent.target.name
+
+    def __getattr__(self, item):
+        def _missing(*args, **kwargs):
+            # print(item, args, kwargs)
+            return None
+        return _missing
+
+def main():
+    a = ModuleVisitor()
+    a.exec(sys.argv[1])
